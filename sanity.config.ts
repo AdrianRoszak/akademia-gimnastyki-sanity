656286071dfafb3ce0681ace50f996f25eb78a12
Weirdo <adroszak@weirdo.codes>
2024-08-12 05:49:34 +0200

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Akademia Gimnastyki',
+
+  projectId: 'ix1jotzh',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
