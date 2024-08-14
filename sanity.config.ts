32e954e604e2619b7e1c589af7fccab11be1162e
Weirdo <adroszak@weirdo.codes>
2024-08-14 05:36:01 +0200

    feat: create new dataset and workspace for development mode

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,15 +5,27 @@ import { schemaTypes } from './schemaTypes'
 
 import { projectId, dataset } from './src/environment'
 
-export default defineConfig({
-  name: 'akademia_gimnastyki',
-  title: 'Akademia Gimnastyki',
-  projectId: projectId || '',
-  dataset: dataset || '',
-
-  plugins: [structureTool(), visionTool()],
-
-  schema: {
-    types: schemaTypes
+export default defineConfig([
+  {
+    name: 'ag-dev',
+    title: 'Akademia Gimnastyki - dev',
+    projectId: projectId || '',
+    dataset: 'development',
+    basePath: '/dev',
+    plugins: [structureTool(), visionTool()],
+    schema: {
+      types: schemaTypes
+    }
+  },
+  {
+    name: 'ag-prod',
+    title: 'Akademia Gimnastyki - prod',
+    projectId: projectId || '',
+    dataset: 'production',
+    basePath: '/prod',
+    plugins: [structureTool(), visionTool()],
+    schema: {
+      types: schemaTypes
+    }
   }
-})
+])
