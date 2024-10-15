a0d026ea94c4648e2d599cbdb8131cbe3fb96a12
Weirdo <adroszak@weirdo.codes>
2024-10-15 05:42:25 +0200

    wip: add faq item content type

---
+++ schemaTypes/content-types/faq-item.ts
@@ -0,0 +1,29 @@
+import { defineField, defineType } from 'sanity'
+
+export const faqItem = defineType({
+  name: 'faq_item',
+  title: 'FAQ',
+  type: 'document',
+  __experimental_formPreviewTitle: false,
+  fields: [
+    defineField({
+      name: 'faq_item_question',
+      title: 'Pytanie',
+      type: 'string',
+      description: 'Krótkie pytanie, które będzie widoczne na stronie FAQ.',
+      validation: (Rule) =>
+        Rule.required()
+          .min(3)
+          .max(120)
+          .warning('Długość tekstu powinna mieć od 3 do 120 znaków')
+    }),
+    defineField({
+      name: 'faq_item_answer',
+      title: 'Odpowiedź',
+      type: 'blockContent',
+      description:
+        'Odpowiedź na pytanie, będzie widoczna po kliknięciu na pytanie.',
+      validation: (Rule) => Rule.required()
+    })
+  ]
+})
