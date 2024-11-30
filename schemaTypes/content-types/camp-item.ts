42a3e694a96946e1c6a04482482df6f95e266359
Weirdo <adroszak@weirdo.codes>
2024-11-30 08:48:41 +0100

    feat: add new field to camp item

--- schemaTypes/content-types/camp-item.ts
+++ schemaTypes/content-types/camp-item.ts
@@ -51,15 +51,63 @@ export const campItem = defineType({
     },
     {
       name: 'camp_item_description',
-      title: 'Opis obozu',
+      title: 'Intro obozu',
       type: 'blockContent',
       validation: (Rule) => Rule.required()
+    },
+    {
+      name: 'camp_item_long_description',
+      title: 'Długi opis',
+      type: 'blockContent',
+      description: 'Dodaj długi opis obozu',
+      validation: (Rule) => Rule.required()
+    },
+    {
+      name: 'camp_item_program',
+      title: 'Program obozu',
+      type: 'blockContent',
+      description: 'Dodaj program obozu'
+    },
+    {
+      name: 'camp_item_price_details',
+      title: 'Szczegóły ceny',
+      type: 'blockContent',
+      description: 'Dodaj elementy zawarte w cenie obozu'
+    },
+    {
+      name: 'camp_item_gallery',
+      title: 'Galeria',
+      type: 'array',
+      of: [{ type: 'image_block' }],
+      description: 'Dodaj galerię zdjęć obozu'
+    },
+    {
+      name: 'camp_item_slug',
+      title: 'Slug',
+      type: 'slug',
+      description: 'Dodaj slug obozu',
+      validation: (Rule) => Rule.required(),
+      options: {
+        source: 'camp_item_name'
+      }
+    },
+    {
+      name: 'camp_item_meta_data_block',
+      title: 'Meta dane',
+      type: 'meta_data',
+      description: 'Dodaj meta dane obozu'
     }
   ],
   preview: {
     select: {
       title: 'camp_item_name',
       media: 'camp_item_image'
+    },
+    prepare(selection) {
+      return {
+        title: selection.title,
+        media: selection.media.image
+      }
     }
   }
 })
