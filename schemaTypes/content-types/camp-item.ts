3262301262f1c09a9691c7cd7b15d0d730d85651
Weirdo <adroszak@weirdo.codes>
2025-01-13 05:41:35 +0100

    refactor: update camp item fields

--- schemaTypes/content-types/camp-item.ts
+++ schemaTypes/content-types/camp-item.ts
@@ -1,45 +1,45 @@
-import { defineType } from 'sanity'
+import { defineField, defineType } from 'sanity'
 
 export const campItem = defineType({
   name: 'camp_item',
   title: 'Obóz',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'camp_item_name',
       title: 'Nazwa obozu',
       type: 'string',
       description: 'Dodaj nazwę obozu',
       validation: (Rule) => Rule.required()
-    },
-    {
+    }),
+    defineField({
       name: 'camp_item_place',
       title: 'Miejsce',
       type: 'string',
       description: 'Dodaj miejsce, w którym odbędzie się obóz',
       validation: (Rule) => Rule.required()
-    },
-    {
+    }),
+    defineField({
       name: 'camp_item_price',
       title: 'Cena',
       type: 'number',
       description: 'Dodaj cenę obozu',
       validation: (Rule) => Rule.required()
-    },
-    {
+    }),
+    defineField({
       name: 'camp_item_image',
       title: 'Zdjęcie',
       type: 'image_block',
       validation: (Rule) => Rule.required()
-    },
-    {
+    }),
+    defineField({
       name: 'camp_item_start_date',
       title: 'Data rozpoczęcia',
       type: 'date',
       description: 'Data rozpoczęcia obozu, format: YYYY-MM-DD',
       validation: (Rule) => Rule.required()
-    },
-    {
+    }),
+    defineField({
       name: 'camp_item_end_date',
       title: 'Data zakończenia',
       type: 'date',
@@ -48,39 +48,46 @@ export const campItem = defineType({
         Rule.required()
           .min(Rule.valueOfField('camp_item_start_date'))
           .error('Data zakończenia musi być późniejsza niż data rozpoczęcia.')
-    },
-    {
+    }),
+    defineField({
       name: 'camp_item_description',
       title: 'Intro obozu',
       type: 'blockContent',
       validation: (Rule) => Rule.required()
-    },
-    {
+    }),
+    defineField({
       name: 'camp_item_long_description',
       title: 'Długi opis',
       type: 'blockContent',
       description: 'Dodaj długi opis obozu'
-    },
-    {
+    }),
+    defineField({
       name: 'camp_item_program',
       title: 'Program obozu',
       type: 'blockContent',
       description: 'Dodaj program obozu'
-    },
-    {
+    }),
+    defineField({
       name: 'camp_item_price_details',
       title: 'Szczegóły ceny',
       type: 'blockContent',
       description: 'Dodaj elementy zawarte w cenie obozu'
-    },
-    {
+    }),
+    defineField({
+      name: 'camp_item_registration',
+      title: 'Formularz zgłoszeniowy',
+      type: 'reference',
+      description: 'Dodaj formularz zgłoszeniowy dla tego wydarzenia',
+      to: [{ type: 'registration_item' }]
+    }),
+    defineField({
       name: 'camp_item_gallery',
       title: 'Galeria',
       type: 'array',
       of: [{ type: 'image_block' }],
       description: 'Dodaj galerię zdjęć obozu'
-    },
-    {
+    }),
+    defineField({
       name: 'camp_item_slug',
       title: 'Slug',
       type: 'slug',
@@ -89,13 +96,13 @@ export const campItem = defineType({
       options: {
         source: 'camp_item_name'
       }
-    },
-    {
+    }),
+    defineField({
       name: 'camp_item_meta_data_block',
       title: 'Meta dane',
       type: 'meta_data',
       description: 'Dodaj meta dane obozu'
-    }
+    })
   ],
   preview: {
     select: {
